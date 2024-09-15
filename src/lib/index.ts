4f043cb2fb1ecfeda3ba0d313590c4720a1f51f3
estatie <estatie@proton.me>
2024-09-16 02:34:39 +0700

    Provide re-export cleint and store libs.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
-// Reexport your entry components here
+export { default as AuthCallback } from './auth/oidc/AuthCallback.svelte';
+export * as client from './auth/oidc/client.js';
+export * as store from './auth/oidc/store.js';
