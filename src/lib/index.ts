892d6068d17311f343bd03503adf21533720e1e4
estatie <estatie@proton.me>
2024-09-16 04:50:03 +0700

    Export as is (give up).

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,3 @@
 export { default as AuthCallback } from './auth/oidc/AuthCallback.svelte';
-export * as auth from './auth/oidc/client.js';
-export * as account from './auth/oidc/store.js';
+export * from './auth/oidc/client.js';
+export * from './auth/oidc/store.js';
